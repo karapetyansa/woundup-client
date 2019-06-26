cfc47498434f1e8219ec82523f56bcbb227a1558
ksa <karapetyan.sa@gmail.com>
2019-06-26 09:25:55 +0300

    Add reset and update icons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,12 +4,16 @@ import deleteIcon from 'assets/deleteIcon.svg'
 import createIcon from 'assets/createIcon.svg'
 import refetchIcon from 'assets/refetchIcon.svg'
 import editIcon from 'assets/editIcon.svg'
+import resetIcon from 'assets/resetIcon.svg'
+import updateIcon from 'assets/updateIcon.svg'
 
 const buttonTypes = {
   delete: deleteIcon,
   create: createIcon,
   edit: editIcon,
   refetch: refetchIcon,
+  reset: resetIcon,
+  update: updateIcon,
 }
 
 export const ActionButton = ({onClick, buttonType, ...rest}) => (
